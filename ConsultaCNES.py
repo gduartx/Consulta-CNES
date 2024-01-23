aa16966f6ba3dc3e8cf470f5514eda803159a160
Gabriel Duarte <gabrielduartecarneiro@gmail.com>
2024-01-22 23:33:27 -0300

    Busca do estabelecimento pelo IBGE + CNES - Retorno dos dados dos estabelecimentos

--- ConsultaCNES.py
+++ ConsultaCNES.py
@@ -13,62 +13,32 @@ navegador.find_element(By.XPATH, '//*[@id="pesquisaValue"]').send_keys('70120305
 navegador.find_element(By.XPATH, '/html/body/div[2]/main/div/div[2]/div/form[3]/div/button').click()
 time.sleep(1)
 navegador.find_element(By.XPATH, '/html/body/div[2]/main/div/div[2]/div/div[4]/table/tbody/tr/td[3]/button[1]').click()
-time.sleep(2)
+time.sleep(1)
 x = True
 ibge = []
 cnes = []
-
+#Percorre toda a tabela de estabelecimentos ativos do profissional
 for i in range(1,15):
     try:
         ibge.append(navegador.find_element(By.XPATH, f'//*[@id="relatorioGeralField"]/tbody[2]/tr[{i}]/td[1]').text)     #Busca IBGE na tabela
         cnes.append(navegador.find_element(By.XPATH, f'//*[@id="relatorioGeralField"]/tbody[2]/tr[{i}]/td[5]').text)     #Busca CNES na tabela
     except:
-        print('break1')
         break
-    
-for i in range(0, len(ibge)-1):
-    print(f"""
-    IBGE{i+1}: {ibge[i]}
-    CNES{i+1}: {cnes[i]}
+#https://cnes2.datasus.gov.br/Mod_Conjunto.asp?VCo_Unidade={IBGE+CNES}
+for i in range(0,len(ibge)-1):
+    url = f'https://cnes2.datasus.gov.br/Mod_Conjunto.asp?VCo_Unidade={ibge[i]+cnes[i]}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        texto = soup.get_text()
+        linhas = texto.split('\n')
+        print(f"""
+        Nome: {linhas[94]}
+        Nome Empresarial: {linhas[104]}
+        CEP: {linhas[131]}
+        Logradouro: {linhas[117]}, {linhas[118]}
+        Complemento: {linhas[129]}
+        Bairro: {linhas[130]}
+        Municipio: {linhas[132]}
+        UF: {linhas[133]}
     """)
-
-
-
-
-"""
-PRIMEIRO CAMPO TABELA:
-IBGE //*[@id="relatorioGeralField"]/tbody[2]/tr[1]/td[1]
-CNES //*[@id="relatorioGeralField"]/tbody[2]/tr[1]/td[5]
-
-SEGUNDO CAMPO TABELA:
-IBGE //*[@id="relatorioGeralField"]/tbody[2]/tr[2]/td[1]
-CNES //*[@id="relatorioGeralField"]/tbody[2]/tr[2]/td[5]
-
-
-"""
-
-
-"""
-PRIMEIRO CAMPO TABELA:
-IBGE //*[@id="relatorioGeralField"]/tbody[2]/tr[1]/td[1]
-CNES //*[@id="relatorioGeralField"]/tbody[2]/tr[1]/td[5]
-
-SEGUNDO CAMPO TABELA:
-IBGE //*[@id="relatorioGeralField"]/tbody[2]/tr[2]/td[1]
-CNES //*[@id="relatorioGeralField"]/tbody[2]/tr[2]/td[5]
-
-TERCEIRO CAMPO TABELA:
-IBGE //*[@id="relatorioGeralField"]/tbody[2]/tr[3]/td[1]
-CNES //*[@id="relatorioGeralField"]/tbody[2]/tr[3]/td[5]
-
-QUARTO CAMPO TABELA:
-IBGE //*[@id="relatorioGeralField"]/tbody[2]/tr[4]/td[1]
-CNES //*[@id="relatorioGeralField"]/tbody[2]/tr[4]/td[5]
-
-QUINTO CAMPO TABELA:
-IBGE //*[@id="relatorioGeralField"]/tbody[2]/tr[5]/td[1]
-CNES //*[@id="relatorioGeralField"]/tbody[2]/tr[5]/td[5]
-
-
-"""
-
